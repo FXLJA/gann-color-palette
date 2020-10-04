2bb4fd2d52f281d4bf627ed981274f7a33072352
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-04 13:34:18 +0700

    Update ColourPickForm

--- src/ColoringGANN/ColourPickForm.java
+++ src/ColoringGANN/ColourPickForm.java
@@ -10,16 +10,17 @@ public class ColourPickForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jColorChooser1 = new javax.swing.JColorChooser();
-        jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
-        jButton2 = new javax.swing.JButton();
+        cmb_tema = new javax.swing.JComboBox<>();
+        colorChooser = new javax.swing.JColorChooser();
+        btn_save = new javax.swing.JButton();
+        lbl_tema = new javax.swing.JLabel();
+        btn_generate = new javax.swing.JButton();
         panelWarna1 = new ColoringGANN.PanelWarna();
         panelWarna2 = new ColoringGANN.PanelWarna();
         panelWarna3 = new ColoringGANN.PanelWarna();
         panelWarna4 = new ColoringGANN.PanelWarna();
         panelWarna5 = new ColoringGANN.PanelWarna();
+        btn_trainingForm = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(570, 480));
@@ -27,34 +28,34 @@ public class ColourPickForm extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(null);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professional", "Colourful", "Monochrome" }));
-        getContentPane().add(jComboBox1);
-        jComboBox1.setBounds(100, 20, 202, 30);
-        getContentPane().add(jColorChooser1);
-        jColorChooser1.setBounds(10, 60, 540, 311);
+        cmb_tema.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professional", "Colourful", "Monochrome" }));
+        getContentPane().add(cmb_tema);
+        cmb_tema.setBounds(100, 20, 202, 30);
+        getContentPane().add(colorChooser);
+        colorChooser.setBounds(10, 60, 540, 311);
 
-        jButton1.setText("Save");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        btn_save.setText("Save");
+        btn_save.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                btn_saveActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1);
-        jButton1.setBounds(10, 410, 120, 23);
+        getContentPane().add(btn_save);
+        btn_save.setBounds(10, 410, 120, 23);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel1.setText("Pilih Tema :");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(20, 30, 80, 10);
+        lbl_tema.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lbl_tema.setText("Pilih Tema :");
+        getContentPane().add(lbl_tema);
+        lbl_tema.setBounds(20, 30, 80, 10);
 
-        jButton2.setText("Generate");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btn_generate.setText("Generate");
+        btn_generate.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btn_generateActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton2);
-        jButton2.setBounds(10, 380, 120, 23);
+        getContentPane().add(btn_generate);
+        btn_generate.setBounds(10, 380, 120, 23);
         getContentPane().add(panelWarna1);
         panelWarna1.setBounds(140, 390, 80, 40);
         getContentPane().add(panelWarna2);
@@ -66,23 +67,38 @@ public class ColourPickForm extends javax.swing.JFrame {
         getContentPane().add(panelWarna5);
         panelWarna5.setBounds(460, 390, 80, 40);
 
+        btn_trainingForm.setText("TrainingForm");
+        btn_trainingForm.setToolTipText("");
+        btn_trainingForm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_trainingFormActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_trainingForm);
+        btn_trainingForm.setBounds(430, 20, 120, 30);
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void btn_saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_saveActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btn_saveActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btn_generateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_generateActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_btn_generateActionPerformed
+
+    private void btn_trainingFormActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_trainingFormActionPerformed
+        Main.trainingFrame.show();
+    }//GEN-LAST:event_btn_trainingFormActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JColorChooser jColorChooser1;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton btn_generate;
+    private javax.swing.JButton btn_save;
+    private javax.swing.JButton btn_trainingForm;
+    private javax.swing.JComboBox<String> cmb_tema;
+    private javax.swing.JColorChooser colorChooser;
+    private javax.swing.JLabel lbl_tema;
     private ColoringGANN.PanelWarna panelWarna1;
     private ColoringGANN.PanelWarna panelWarna2;
     private ColoringGANN.PanelWarna panelWarna3;
