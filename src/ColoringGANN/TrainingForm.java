617d9f4101bf1b8d1e245c9989c4a818d494e2c0
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-04 16:08:34 +0700

    Update traningForm

--- src/ColoringGANN/TrainingForm.java
+++ src/ColoringGANN/TrainingForm.java
@@ -1,138 +1,174 @@
 package ColoringGANN;
 
-public class TrainingForm extends javax.swing.JFrame {
+import java.awt.Color;
+import java.io.File;
+import javax.swing.JFileChooser;
 
+public class TrainingForm extends javax.swing.JFrame {
+    public GeneticTrainer geneticTrainer;
+    
     public TrainingForm() {
         initComponents();
+        
+        geneticTrainer = new GeneticTrainer(sl_trainingRatio.getValue()/100f, sl_populationSize.getValue(), sl_mutationRate.getValue()/100f);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        panelWarna1 = new ColoringGANN.PanelWarna();
-        panelWarna2 = new ColoringGANN.PanelWarna();
-        panelWarna3 = new ColoringGANN.PanelWarna();
-        panelWarna4 = new ColoringGANN.PanelWarna();
-        panelWarna5 = new ColoringGANN.PanelWarna();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jLabel1 = new javax.swing.JLabel();
-        jButton6 = new javax.swing.JButton();
-        jButton7 = new javax.swing.JButton();
-        jSlider1 = new javax.swing.JSlider();
-        jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
-        jSlider2 = new javax.swing.JSlider();
-        jLabel3 = new javax.swing.JLabel();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jLabel4 = new javax.swing.JLabel();
-        jSlider3 = new javax.swing.JSlider();
+        btn_load = new javax.swing.JButton();
+        btn_save = new javax.swing.JButton();
+        btn_generate = new javax.swing.JButton();
+        sl_trainingRatio = new javax.swing.JSlider();
+        lbl_trainingRatio = new javax.swing.JLabel();
+        ed_trainingRatio = new javax.swing.JTextField();
+        sl_populationSize = new javax.swing.JSlider();
+        lbl_populationSize = new javax.swing.JLabel();
+        ed_populationSize = new javax.swing.JTextField();
+        ed_mutationRate = new javax.swing.JTextField();
+        lbl_mutationRate = new javax.swing.JLabel();
+        sl_mutationRate = new javax.swing.JSlider();
+        btn_autoTrain1 = new javax.swing.JButton();
+        btn_nextGen1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMinimumSize(new java.awt.Dimension(497, 350));
-        setPreferredSize(new java.awt.Dimension(497, 350));
+        setMinimumSize(new java.awt.Dimension(500, 260));
+        setPreferredSize(new java.awt.Dimension(500, 260));
+        setResizable(false);
         getContentPane().setLayout(null);
 
-        jButton3.setText("Load");
-        getContentPane().add(jButton3);
-        jButton3.setBounds(20, 20, 80, 23);
+        btn_load.setText("Load");
+        btn_load.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_loadActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_load);
+        btn_load.setBounds(20, 20, 80, 23);
 
-        jButton4.setText("Save");
-        getContentPane().add(jButton4);
-        jButton4.setBounds(110, 20, 90, 23);
+        btn_save.setText("Save");
+        getContentPane().add(btn_save);
+        btn_save.setBounds(110, 20, 90, 23);
 
-        jButton5.setText("Auto Train");
-        getContentPane().add(jButton5);
-        jButton5.setBounds(380, 290, 90, 23);
-        getContentPane().add(panelWarna1);
-        panelWarna1.setBounds(30, 220, 80, 40);
-        getContentPane().add(panelWarna2);
-        panelWarna2.setBounds(120, 220, 80, 40);
-        getContentPane().add(panelWarna3);
-        panelWarna3.setBounds(210, 220, 80, 40);
-        getContentPane().add(panelWarna4);
-        panelWarna4.setBounds(300, 220, 80, 40);
-        getContentPane().add(panelWarna5);
-        panelWarna5.setBounds(390, 220, 80, 40);
+        btn_generate.setText("Generate");
+        btn_generate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_generateActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_generate);
+        btn_generate.setBounds(270, 180, 90, 30);
+        getContentPane().add(sl_trainingRatio);
+        sl_trainingRatio.setBounds(160, 60, 210, 22);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Professional", "Colourful", "Monochrome" }));
-        getContentPane().add(jComboBox1);
-        jComboBox1.setBounds(110, 180, 202, 30);
+        lbl_trainingRatio.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lbl_trainingRatio.setText("Training/Test Ratio");
+        getContentPane().add(lbl_trainingRatio);
+        lbl_trainingRatio.setBounds(20, 60, 130, 20);
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel1.setText("Pilih Tema :");
-        getContentPane().add(jLabel1);
-        jLabel1.setBounds(30, 190, 80, 10);
+        ed_trainingRatio.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        getContentPane().add(ed_trainingRatio);
+        ed_trainingRatio.setBounds(380, 60, 90, 30);
 
-        jButton6.setText("Bagus");
-        getContentPane().add(jButton6);
-        jButton6.setBounds(30, 290, 73, 23);
+        sl_populationSize.setMaximum(10000);
+        sl_populationSize.setMinimum(100);
+        sl_populationSize.setValue(5000);
+        getContentPane().add(sl_populationSize);
+        sl_populationSize.setBounds(160, 100, 210, 22);
 
-        jButton7.setText("Kurang");
-        getContentPane().add(jButton7);
-        jButton7.setBounds(120, 290, 67, 23);
-        getContentPane().add(jSlider1);
-        jSlider1.setBounds(160, 60, 210, 22);
+        lbl_populationSize.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lbl_populationSize.setText("Population Size");
+        getContentPane().add(lbl_populationSize);
+        lbl_populationSize.setBounds(20, 100, 110, 20);
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel2.setText("Training/Test Ratio");
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(20, 60, 130, 20);
+        ed_populationSize.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        getContentPane().add(ed_populationSize);
+        ed_populationSize.setBounds(380, 100, 90, 30);
 
-        jTextField2.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(jTextField2);
-        jTextField2.setBounds(380, 60, 90, 30);
-        getContentPane().add(jSlider2);
-        jSlider2.setBounds(130, 100, 240, 22);
+        ed_mutationRate.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        getContentPane().add(ed_mutationRate);
+        ed_mutationRate.setBounds(380, 140, 90, 30);
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel3.setText("Population Size");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(20, 100, 110, 20);
+        lbl_mutationRate.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        lbl_mutationRate.setText("Mutation Rate");
+        getContentPane().add(lbl_mutationRate);
+        lbl_mutationRate.setBounds(20, 140, 110, 20);
 
-        jTextField3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(jTextField3);
-        jTextField3.setBounds(380, 100, 90, 30);
+        sl_mutationRate.setValue(10);
+        getContentPane().add(sl_mutationRate);
+        sl_mutationRate.setBounds(160, 140, 210, 22);
 
-        jTextField4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(jTextField4);
-        jTextField4.setBounds(380, 140, 90, 30);
+        btn_autoTrain1.setText("Auto Train");
+        btn_autoTrain1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_autoTrain1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_autoTrain1);
+        btn_autoTrain1.setBounds(150, 180, 100, 30);
 
-        jLabel4.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel4.setText("Mutation Rate");
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(20, 140, 110, 20);
-        getContentPane().add(jSlider3);
-        jSlider3.setBounds(130, 140, 240, 22);
+        btn_nextGen1.setText("Next Gen");
+        btn_nextGen1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_nextGen1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_nextGen1);
+        btn_nextGen1.setBounds(20, 180, 110, 30);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btn_loadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_loadActionPerformed
+        JFileChooser fileChooser = new JFileChooser();
+        
+        int result = fileChooser.showOpenDialog(this);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            geneticTrainer.dataset.loadDataset(selectedFile);
+        }
+    }//GEN-LAST:event_btn_loadActionPerformed
+
+    private void btn_generateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_generateActionPerformed
+        System.out.println("Professional : ");
+        System.out.print("#FF0000");printColors(geneticTrainer.bestANN.generateColor(0, Color.red));
+        System.out.print("#00FF00");printColors(geneticTrainer.bestANN.generateColor(0, Color.green));
+        System.out.print("#0000FF");printColors(geneticTrainer.bestANN.generateColor(0, Color.blue));
+        System.out.print("#000000");printColors(geneticTrainer.bestANN.generateColor(0, Color.black));
+        System.out.print("#FFFFFF");printColors(geneticTrainer.bestANN.generateColor(0, Color.white));
+    }//GEN-LAST:event_btn_generateActionPerformed
+    
+    private void printColors(Color[] c){
+        for(int i=0; i<c.length; i++) {
+            System.out.print(String.format("#%06x", c[i].getRGB() & 0x00FFFFFF).toUpperCase());
+        }
+        System.out.println();
+    }
+    
+    private void btn_nextGen1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_nextGen1ActionPerformed
+        geneticTrainer.nextGen();
+    }//GEN-LAST:event_btn_nextGen1ActionPerformed
+
+    private void btn_autoTrain1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_autoTrain1ActionPerformed
+        for(int i=0; i<100; i++) {
+            geneticTrainer.nextGen();
+        }
+    }//GEN-LAST:event_btn_autoTrain1ActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
-    private javax.swing.JButton jButton7;
-    private javax.swing.JComboBox<String> jComboBox1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JSlider jSlider1;
-    private javax.swing.JSlider jSlider2;
-    private javax.swing.JSlider jSlider3;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private ColoringGANN.PanelWarna panelWarna1;
-    private ColoringGANN.PanelWarna panelWarna2;
-    private ColoringGANN.PanelWarna panelWarna3;
-    private ColoringGANN.PanelWarna panelWarna4;
-    private ColoringGANN.PanelWarna panelWarna5;
+    private javax.swing.JButton btn_autoTrain1;
+    private javax.swing.JButton btn_generate;
+    private javax.swing.JButton btn_load;
+    private javax.swing.JButton btn_nextGen1;
+    private javax.swing.JButton btn_save;
+    private javax.swing.JTextField ed_mutationRate;
+    private javax.swing.JTextField ed_populationSize;
+    private javax.swing.JTextField ed_trainingRatio;
+    private javax.swing.JLabel lbl_mutationRate;
+    private javax.swing.JLabel lbl_populationSize;
+    private javax.swing.JLabel lbl_trainingRatio;
+    private javax.swing.JSlider sl_mutationRate;
+    private javax.swing.JSlider sl_populationSize;
+    private javax.swing.JSlider sl_trainingRatio;
     // End of variables declaration//GEN-END:variables
 }
