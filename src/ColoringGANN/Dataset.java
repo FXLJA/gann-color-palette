2bdce952f8f068f3ed85d62b1d60785bd706e804
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-04 12:40:34 +0700

    Update Dataset.java

--- src/ColoringGANN/Dataset.java
+++ src/ColoringGANN/Dataset.java
@@ -1,12 +1,47 @@
 package ColoringGANN;
 
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Dataset {
     private ArrayList<ColorData> dataList = new ArrayList<ColorData>();
     
     public void loadDataset(String path) {
-        // TODO
+        try {
+            Scanner sc = new Scanner(new File(path));
+            sc.useDelimiter(",");
+            while (sc.hasNext()) {
+                ColorData c = new ColorData();
+                
+                c.mode = modeToIndex(sc.next());
+                c.warnaDominan = Color.decode(sc.next());
+                c.warnaTerang = Color.decode(sc.next());
+                c.warnaMid = Color.decode(sc.next());
+                c.warnaDark1 = Color.decode(sc.next());
+                c.warnaDark2 = Color.decode(sc.next());
+                
+                dataList.add(c);
+            }  
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    
+    private int modeToIndex(String mode){
+        switch(mode) {
+            case "p":
+                return 0;
+            case "c":
+                return 1;
+            case "m":
+                return 2;
+        }
+        System.out.println("Error: Couldn't parse " + mode + "into index");
+        return -1;
     }
     
     public ColorData get(int index) {
