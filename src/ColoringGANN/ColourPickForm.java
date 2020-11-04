5343a38840b96c11e77649aea43e10676cb81aaf
gregetexe <53731848+gregetexe@users.noreply.github.com>
2020-11-04 15:25:45 +0700

    hexcode minor fix

--- src/ColoringGANN/ColourPickForm.java
+++ src/ColoringGANN/ColourPickForm.java
@@ -17,13 +17,9 @@ public class ColourPickForm extends javax.swing.JFrame {
         btn_save = new javax.swing.JButton();
         lbl_tema = new javax.swing.JLabel();
         btn_generate = new javax.swing.JButton();
-        panelWarna1 = new ColoringGANN.PanelWarna();
-        panelWarna2 = new ColoringGANN.PanelWarna();
-        panelWarna3 = new ColoringGANN.PanelWarna();
-        panelWarna4 = new ColoringGANN.PanelWarna();
-        panelWarna5 = new ColoringGANN.PanelWarna();
         btn_trainingForm = new javax.swing.JButton();
         btn_printHex = new javax.swing.JButton();
+        HexField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(570, 480));
@@ -35,7 +31,7 @@ public class ColourPickForm extends javax.swing.JFrame {
         getContentPane().add(cmb_tema);
         cmb_tema.setBounds(100, 20, 202, 30);
         getContentPane().add(colorChooser);
-        colorChooser.setBounds(10, 60, 540, 311);
+        colorChooser.setBounds(10, 60, 560, 311);
 
         btn_save.setText("Save");
         btn_save.addActionListener(new java.awt.event.ActionListener() {
@@ -49,7 +45,7 @@ public class ColourPickForm extends javax.swing.JFrame {
         lbl_tema.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         lbl_tema.setText("Pilih Tema :");
         getContentPane().add(lbl_tema);
-        lbl_tema.setBounds(20, 30, 80, 10);
+        lbl_tema.setBounds(20, 20, 80, 30);
 
         btn_generate.setText("Generate");
         btn_generate.addActionListener(new java.awt.event.ActionListener() {
@@ -59,18 +55,8 @@ public class ColourPickForm extends javax.swing.JFrame {
         });
         getContentPane().add(btn_generate);
         btn_generate.setBounds(10, 380, 120, 22);
-        getContentPane().add(panelWarna1);
-        panelWarna1.setBounds(140, 390, 80, 40);
-        getContentPane().add(panelWarna2);
-        panelWarna2.setBounds(220, 390, 80, 40);
-        getContentPane().add(panelWarna3);
-        panelWarna3.setBounds(300, 390, 80, 40);
-        getContentPane().add(panelWarna4);
-        panelWarna4.setBounds(380, 390, 80, 40);
-        getContentPane().add(panelWarna5);
-        panelWarna5.setBounds(460, 390, 80, 40);
 
-        btn_trainingForm.setText("TrainingForm");
+        btn_trainingForm.setText("Training Form");
         btn_trainingForm.setToolTipText("");
         btn_trainingForm.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -89,6 +75,12 @@ public class ColourPickForm extends javax.swing.JFrame {
         });
         getContentPane().add(btn_printHex);
         btn_printHex.setBounds(320, 20, 100, 30);
+
+        HexField.setEditable(false);
+        HexField.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        HexField.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        getContentPane().add(HexField);
+        HexField.setBounds(140, 380, 110, 50);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -112,11 +104,11 @@ public class ColourPickForm extends javax.swing.JFrame {
         String GValue = Integer.toHexString(rgbColor.getGreen()).toUpperCase();
         String BValue = Integer.toHexString(rgbColor.getBlue()).toUpperCase();
         String Hex = RValue + GValue + BValue;
-        
-        System.out.println(Hex);
+        HexField.setText(Hex);
     }//GEN-LAST:event_btn_printHexActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField HexField;
     private javax.swing.JButton btn_generate;
     private javax.swing.JButton btn_printHex;
     private javax.swing.JButton btn_save;
@@ -124,11 +116,6 @@ public class ColourPickForm extends javax.swing.JFrame {
     private javax.swing.JComboBox<String> cmb_tema;
     private javax.swing.JColorChooser colorChooser;
     private javax.swing.JLabel lbl_tema;
-    private ColoringGANN.PanelWarna panelWarna1;
-    private ColoringGANN.PanelWarna panelWarna2;
-    private ColoringGANN.PanelWarna panelWarna3;
-    private ColoringGANN.PanelWarna panelWarna4;
-    private ColoringGANN.PanelWarna panelWarna5;
     // End of variables declaration//GEN-END:variables
 
 }
